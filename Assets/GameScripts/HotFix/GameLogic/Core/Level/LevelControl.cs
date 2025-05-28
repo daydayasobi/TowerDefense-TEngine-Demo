3a1c98ef1ac075f8b17f95408400eeb3e13358ad
Salieri-Z <zhaozulin39@gmail.com>
2025-05-29 01:52:39 +0800

    测试按钮生成一个炮塔

--- Assets/GameScripts/HotFix/GameLogic/Core/Level/LevelControl.cs
+++ Assets/GameScripts/HotFix/GameLogic/Core/Level/LevelControl.cs
@@ -9,7 +9,11 @@ namespace GameLogic
     {
         private LevelManager levelManager;
         private CameraInput cameraInput;
-        
+        private GameObject roomRoot;
+
+        private bool isBuilding = false;
+        private bool pause = false;
+
         public void OnEnter()
         {
             GameModule.UI.ShowUI<LevelMainInfoUI>();
@@ -17,32 +21,48 @@ namespace GameLogic
 
         public void Update()
         {
-            
+            if (isBuilding)
+            {
+            }
+            else
+            {
+            }
         }
 
         public void ShowPreviewTower()
         {
-            Log.Info("1111111111111111111");
+            var gameObject = PoolManager.Instance.GetGameObject("AssaultCannon_Level1", parent: roomRoot.transform);
+
+            isBuilding = true;
         }
-        
+
         public void StartWave()
         {
-            
         }
 
         public void Pause()
         {
-            
+            pause = true;
         }
-        
-        public static LevelControl Create(LevelManager levelPathManager, CameraInput cameraInput)
+
+        public void Resume()
+        {
+            pause = false;
+        }
+
+        public static LevelControl Create(GameObject roomRoot, LevelManager levelPathManager, CameraInput cameraInput)
         {
             // LevelControl levelControl = ReferencePool.Acquire<LevelControl>();
             LevelControl levelControl = new LevelControl();
             // levelControl.level = level;
+            levelControl.roomRoot = roomRoot;
             levelControl.levelManager = levelPathManager;
             levelControl.cameraInput = cameraInput;
             return levelControl;
+        }
+
+        public void Clear()
+        {
         }
     }
 }
--- Assets/GameScripts/HotFix/GameLogic/Core/LevelSystem.cs
+++ Assets/GameScripts/HotFix/GameLogic/Core/LevelSystem.cs
@@ -9,39 +9,44 @@ namespace GameLogic
     public class LevelSystem : Singleton<LevelSystem>
     {
         private LevelControl levelControl;
+        private GameObject _roomRoot;
 
         protected override void OnInit()
         {
             base.OnInit();
+            Utility.Unity.AddUpdateListener(Update);
         }
 
         public async UniTaskVoid LoadLevelSystem(string levelName)
         {
             await GameModule.Scene.LoadSceneAsync(levelName);
-            
+
+            // 创建房间根对象
+            _roomRoot = new GameObject("BattleRoom");
+
             LevelManager levelPathManager = UnityEngine.GameObject.Find("LevelManager").GetComponent<LevelManager>();
             if (levelPathManager == null)
             {
                 Log.Error("Can not find LevelPathManager instance in scene");
                 return;
             }
-            
+
             CameraInput cameraInput = UnityEngine.GameObject.Find("GameCamera").GetComponent<CameraInput>();
             if (cameraInput == null)
             {
                 Log.Error("Can not find CameraInput instance in scene");
                 return;
             }
-            
-            levelControl = LevelControl.Create( levelPathManager, cameraInput);
-            
+
+            levelControl = LevelControl.Create(_roomRoot, levelPathManager, cameraInput);
+
             // 监听游戏事件
             GameEvent.AddEventListener(EventDefine.ChangeScene, OnChangeScene);
             GameEvent.AddEventListener(EventDefine.LoadLevel, OnLoadLevel);
             GameEvent.AddEventListener(EventDefine.ReloadLevel, OnReLoadLevel);
             GameEvent.AddEventListener(EventDefine.GameOver, OnGameOver);
             GameEvent.AddEventListener(EventDefine.ShowPreviewTower, OnShowPreviewTower);
-            
+
             levelControl.OnEnter();
         }
 
@@ -54,24 +59,26 @@ namespace GameLogic
             GameEvent.RemoveEventListener(EventDefine.ShowPreviewTower, OnShowPreviewTower);
         }
 
+        public void Update()
+        {
+            if (levelControl != null)
+                levelControl.Update();
+        }
+
         private void OnChangeScene()
         {
-            
         }
 
         private void OnLoadLevel()
         {
-            
         }
-        
+
         private void OnReLoadLevel()
         {
-            
         }
-        
+
         private void OnGameOver()
         {
-            
         }
 
         private void OnShowPreviewTower()
